fe115998fdbe98f0b69f9b793af6aceeb3df4f10
Daniel Molist <Daniel.Molist@gmail.com>
2024-03-11 13:44:32 +0100

    Fin sección 9

---
+++ src/app/countries/components/country-table/country-table.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'countries-country-table',
+  templateUrl: './country-table.component.html'
+})
+export class CountryTableComponent {
+  @Input()
+  public countries: Country[] = [];
+}
